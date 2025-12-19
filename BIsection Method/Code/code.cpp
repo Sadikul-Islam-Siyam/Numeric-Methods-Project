6934ff2c1c39868fb32fc4c9e9420706aa25b749
Saleh Sadid Mir <salehsadid16@gmail.com>
2025-12-19 12:33:43 +0600

    Refactor input handling and modified bisection

--- BIsection Method/Code/code.cpp
+++ BIsection Method/Code/code.cpp
@@ -25,36 +25,34 @@ int main()
         return 1;
     }
     
-    // Read coefficients and error tolerance from input file
+    // coefficients and error tolerance from input file
     input >> an1 >> an2 >> an3 >> an4;
-    long double e;
+    long double e,h;
     input >> e;
-    
-    output << "Equation: f(x) = (" << an1 << ")x^4 + (" << an2 << ")x^3 + (" << an3 << ")x^2 + (" << an4 << ")x" << endl;
+    input >> h;
+    output << "The equation is: f(x) = (" << an1 << ")x^4 + (" << an2 << ")x^3 + (" << an3 << ")x^2 + (" << an4 << ")x" << endl;
     output << endl;
-    output << "Coefficients are: " << an1 << " " << an2 << " " << an3 << " " << an4 << endl;
     output << "Error tolerance: " << e << endl;
     output << endl;
     
-    // Find ranges where roots exist
+    // finding the range for roots
     vector<pair<long double, long double>> range;
     long double range1 = -abs(xmax());
     long double range2 = abs(xmax());
     
     while(range1 <= range2)
     {
-        if((func(range1) * func(range1 + 0.5)) < 0)
+        if((func(range1) * func(range1 + h)) < 0)
         {
-            range.push_back({range1, range1 + 0.5});
-            output << "Range found: " << range1 << " to " << range1 + 0.5 << endl;
+            range.push_back({range1, range1 + h});
+            output << "Range found: [" << range1 << " , " << range1 + h << "]" << endl;
         }
-        range1 += 0.5;
+        range1 += h; // we are taking h as input step size
     }
     output << endl;
     
-    // Apply Bisection Method for each range
-    for(int i = 0; i < (int)range.size(); i++)
-    {
+    // Bisection Method for each range
+    for(int i = 0; i < (int)range.size(); i++){
         int it = 0;
         long double a = range[i].first;
         long double b = range[i].second;
@@ -63,27 +61,23 @@ int main()
         output << "Finding Root " << i + 1 << ":" << endl;
         output << fixed << setprecision(6);
         
-        while(abs((b - a) / 2) > e)
-        {
+        while(abs((b-a)/2)>e){
             it++;
             c = (a + b) / 2;
             
-            if(func(c) == 0.0)
-            {
+            if(func(c) == 0.0){
                 break;
             }
-            else if((func(a) * func(c)) < 0)
-            {
+            else if((func(a) * func(c)) < 0){
                 b = c;
             }
-            else
-            {
+            else{
                 a = c;
             }
         }
         
         output << "Root " << i + 1 << ": " << c << endl;
-        output << "Iterations: " << it << endl;
+        output << "Number of iterations: " << it << endl;
         output << endl;
     }
     
